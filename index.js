9f03a079cf627f8ec96fba44ba74f3cb7440e147
Mahamududl Hasan <m757162@gmail.com>
2024-06-21 11:25:30 +0600

    updated

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
+const auth = require("json-server-auth");
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = 3001; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 
 server.listen(port);
